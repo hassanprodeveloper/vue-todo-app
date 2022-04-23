e944543dc57151c9dd7d53e7de989b3bdeb9f7ca
hassanakram-n <hassanakram.n@gmail.com>
2022-04-24 02:39:06 +0500

    bootstrap add and comfigured

--- src/main.js
+++ src/main.js
@@ -3,4 +3,11 @@ import App from "./App.vue";
 import router from "./router";
 import store from "./store";
 
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap";
+import "core-js/stable";
+import "regenerator-runtime/runtime";
+import "@babel/polyfill";
+import 'intersection-observer' 
+
 createApp(App).use(store).use(router).mount("#app");
