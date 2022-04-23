0442cc0c127b677ecc09803c296ef551b865c1ee
hassanakram-n <hassanakram.n@gmail.com>
2022-04-24 01:41:19 +0500

    working on navbar

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,22 @@
 <template>
-  <nav>
-    <router-link to="/">Welcome</router-link> |
-    <router-link to="/login">Login</router-link> |
-    <router-link to="/signup">Signup</router-link> |
-    <router-link to="/todos">Todos</router-link>
-  </nav>
-  <router-view />
+  <div>
+    <Navbar />
+    <nav>
+      <router-link to="/">Welcome</router-link> |
+      <router-link to="/login">Login</router-link> |
+      <router-link to="/signup">Signup</router-link> |
+      <router-link to="/todos">Todos</router-link>
+    </nav>
+    <router-view />
+  </div>
 </template>
 
-
+<script>
+import Navbar from "@/components/Navbar.vue";
+export default {
+  name: "AppView",
+  components: {
+    Navbar,
+  },
+};
+</script>
