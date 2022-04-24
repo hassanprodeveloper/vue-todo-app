e613e2592dd734defb38a10baefc7d68e45cea21
hassanakram-n <hassanakram.n@gmail.com>
2022-04-25 00:15:37 +0500

    navbar code refactored

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -2,6 +2,7 @@
   <nav class="navbar navbar-expand-lg navbar-light bg-light">
     <div class="container">
       <router-link class="navbar-brand" to="/">TODO APP</router-link>
+
       <button
         class="navbar-toggler"
         type="button"
@@ -13,11 +14,13 @@
       >
         <span class="navbar-toggler-icon"></span>
       </button>
+
       <div class="collapse navbar-collapse" id="navbarTogglerDemo02">
         <ul class="navbar-nav ms-auto" v-if="isLoggedIn">
           <li class="nav-item">
             <router-link class="nav-link" to="/todos">Todos</router-link>
           </li>
+
           <li class="nav-item">
             <button class="btn btn-primary" v-on:click="logoutHandler">
               Logout
@@ -29,6 +32,7 @@
           <li class="nav-item">
             <router-link class="nav-link" to="/login">Login</router-link>
           </li>
+
           <li class="nav-item">
             <router-link class="nav-link" to="/signup">Sign up</router-link>
           </li>
@@ -43,18 +47,20 @@ import { mapActions, mapGetters } from "vuex";
 
 export default {
   name: "NavbarComponent",
+
   computed: {
     ...mapGetters({ User: "StateUser" }),
+
     isLoggedIn: function () {
       return this.$store.getters.isAuthenticated;
     },
   },
+
   methods: {
     ...mapActions(["LogoutAction"]),
+
     async logoutHandler() {
       const response = await this.LogoutAction();
-
-      console.log("logout handler response", response);
 
       if (!response) {
         alert("Something went wrong, please try again");
@@ -66,5 +72,4 @@ export default {
   },
 };
 </script>
-<style >
-</style>
+ 
