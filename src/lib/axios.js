01530ddf267e57e74d12fbc1d6c17bbf55cfdb0d
hassanakram-n <hassanakram.n@gmail.com>
2022-04-25 00:22:22 +0500

    axios functions code refactored

--- src/lib/axios.js
+++ src/lib/axios.js
@@ -11,43 +11,21 @@ export const API = axios.create({
 
 export const postFormData = async (API_END_POINT, data) => {
   const token = store.state.auth.user?.token;
-  console.log("post form data token", token);
 
   try {
     const response = await API.post(API_END_POINT, data, {
       headers: {
         "Content-Type": "multipart/form-data",
-        Authorization: token ? `Bearer ${token}` : "",
+        "Authorization": token ? `Bearer ${token}` : "",
       },
     });
 
     return response.data;
   } catch (error) {
-    console.log(`postFormData ${API_END_POINT} catch error`, error);
-    let errorCode = String(error).includes("401") ? "Unauthorized" : null;
-    return errorCode;
+    catchBlockErrorHandler(error);
   }
 };
 
-export const putFormData = async (API_END_POINT, data) => {
-  const token = store.state.auth.user?.token;
-  console.log("put form data token", token);
-
-  try {
-    const response = await API.put(API_END_POINT, data, {
-      headers: {
-        "Content-Type": "multipart/form-data",
-        Authorization: token ? `Bearer ${token}` : "",
-      },
-    });
-
-    return response.data;
-  } catch (error) {
-    console.log(`putFormData ${API_END_POINT} catch error`, error);
-    let errorCode = String(error).includes("401") ? "Unauthorized" : null;
-    return errorCode;
-  }
-};
 export const getData = async (API_END_POINT) => {
   const token = store.state.auth.user?.token;
 
@@ -55,54 +33,51 @@ export const getData = async (API_END_POINT) => {
     const response = await API.get(API_END_POINT, {
       headers: {
         "Content-Type": "multipart/form-data",
-        Authorization: token ? `Bearer ${token}` : "",
+        "Authorization": token ? `Bearer ${token}` : "",
       },
     });
 
     return response.data;
   } catch (error) {
-    console.log(`getData ${API_END_POINT} catch error`, error);
-    let errorCode = String(error).includes("401") ? "Unauthorized" : null;
-    return errorCode;
+    catchBlockErrorHandler(error);
   }
 };
 
 export const putData = async (API_END_POINT, data) => {
   const token = store.state.auth.user?.token;
-  console.log("put data token", token);
 
   try {
     const response = await API.put(API_END_POINT, data, {
       headers: {
         "Content-Type": "application/json",
-        Authorization: token ? `Bearer ${token}` : "",
+        "Authorization": token ? `Bearer ${token}` : "",
       },
     });
 
     return response.data;
   } catch (error) {
-    console.log(`put todo ${API_END_POINT} catch error`, error);
-    let errorCode = String(error).includes("401") ? "Unauthorized" : null;
-    return errorCode;
+    catchBlockErrorHandler(error);
   }
 };
 
 export const deleteData = async (API_END_POINT) => {
   const token = store.state.auth.user?.token;
-  console.log("delete data token", token);
 
   try {
     await API.delete(API_END_POINT, {
       headers: {
         "Content-Type": "application/json",
-        Authorization: token ? `Bearer ${token}` : "",
+        "Authorization": token ? `Bearer ${token}` : "",
       },
     });
 
     return true;
   } catch (error) {
-    console.log(`delete todo ${API_END_POINT} catch error`, error);
-    let errorCode = String(error).includes("401") ? "Unauthorized" : null;
-    return errorCode;
+    catchBlockErrorHandler(error);
   }
+};
+
+const catchBlockErrorHandler = (error) => {
+  let errorCode = String(error).includes("401") ? "Unauthorized" : null;
+  return errorCode;
 };
