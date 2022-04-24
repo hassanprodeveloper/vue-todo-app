c69284b9e6a72fa6ca395cf3154028496ab1cad6
hassanakram-n <hassanakram.n@gmail.com>
2022-04-24 17:44:35 +0500

    todos page completed

---
+++ src/components/TodosPagination.vue
@@ -0,0 +1,33 @@
+<template >
+  <div class="d-flex justify-content-center align-items-center paginationContainer">
+    <nav aria-label="...">
+      <ul class="pagination">
+        <li
+          v-for="page in pagination"
+          :key="page.label"
+          v-bind:class="page.url ? 'page-item' : 'page-item disabled'"
+        >
+          <a class="page-link" href="#">{{ page.label }}</a>
+        </li>
+      </ul>
+    </nav>
+  </div>
+</template>
+<script>
+import { mapGetters } from "vuex";
+export default {
+  name: "TodosPagination",
+  computed: {
+    ...mapGetters({
+      pagination: "Pagination",
+    }),
+  },
+};
+</script>
+ 
+ <style>
+.paginationContainer {
+  width: 100%;
+  margin: 2rem 0px;
+}
+</style>
--- src/views/TodosView.vue
+++ src/views/TodosView.vue
@@ -1,11 +1,88 @@
 <template>
   <div>
-    <h1>Todos page</h1>
+    <div class="card">
+      <div class="card-body p-4">
+        <h1 class="text-center my-3 pb-3">To Do List</h1>
+
+        <table class="table mb-4">
+          <thead>
+            <tr>
+              <th scope="col">No.</th>
+              <th scope="col">Todo item</th>
+              <th scope="col">Actions</th>
+            </tr>
+          </thead>
+
+          <tbody>
+            <tr v-for="(todo, index) in todos" :key="todo.id">
+              <th scope="row">{{ index + 1 }}</th>
+              <td>{{ todo.title }}</td>
+              <td>
+                <button type="button" class="btn btn-danger">Delete</button>
+                <button
+                  type="button"
+                  class="btn btn-success ms-1"
+                  @click="handleEdit(todo)"
+                >
+                  Edit
+                </button>
+              </td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+    </div>
+
+    <TodosPagination />
+
+    <router-link to="/todo/create">
+      <button class="btn btn-primary fabButton">Add</button></router-link
+    >
   </div>
 </template>
 
 <script>
+import { mapActions, mapGetters } from "vuex";
+
+import TodosPagination from "@/components/TodosPagination.vue";
+
 export default {
   name: "TodosView",
+  components: {
+    TodosPagination,
+  },
+  computed: {
+    ...mapGetters({
+      todos: "TodosList",
+    }),
+  },
+
+  methods: {
+    ...mapActions(["GetTodosListAction"]),
+
+    handleEdit(todo) {
+      this.$router.push({ path: "/todo/update", query: { ...todo } });
+    },
+  },
+
+  created() {
+    this.GetTodosListAction();
+  },
 };
 </script>
+
+<style scoped>
+.fabButton {
+  position: absolute;
+  bottom: 1rem;
+  right: 1rem;
+
+  width: 5rem;
+  height: 5rem;
+  border-radius: 50%;
+}
+.card {
+  margin: 2rem;
+  border: none !important;
+}
+</style>
